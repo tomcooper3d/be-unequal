aafc409e56361fbaf2fa6d0910b926719cc2a963
Connor Bell <connor.n.bell@gmail.com>
2014-07-05 18:42:37 +0100

    trigger stuff not done

--- Assets/Scripts/WallTrigger.cs
+++ Assets/Scripts/WallTrigger.cs
@@ -11,18 +11,32 @@ public class WallTrigger : MonoBehaviour {
 	public GameObject leftColliderObject;
 	public GameObject rightColliderObject;
 
+	private bool transitionInEffect = false;
+
 	void Start () {
 		mapParent = mapParentObject.GetComponent<MapParent> ();
 	}
-	void OnTriggerExit(Collider collisionInfo) {
+	void OnTriggerEnter(Collider collisionInfo) {
+		if (transitionInEffect) return;
 		if (collisionInfo.gameObject == topColliderObject) {
+			transitionInEffect = true;
+			transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
 			mapParent.moveTop ();
 		} else if (collisionInfo.gameObject == bottomColliderObject) {
+			transitionInEffect = true;
+			transform.position = new Vector3(-transform.position.x, -transform.position.y, transform.position.z);
 			mapParent.moveBottom ();
 		} else if (collisionInfo.gameObject == leftColliderObject) {
+			transitionInEffect = true;
 			mapParent.moveLeft ();
 		} else if (collisionInfo.gameObject == rightColliderObject) {
+			transitionInEffect = true;
 			mapParent.moveRight ();
 		} else { } 
+	}
+	void OnTriggerExit(Collider collisionInfo) {
+		if (collisionInfo.gameObject == topColliderObject || collisionInfo.gameObject == bottomColliderObject || collisionInfo.gameObject == leftColliderObject || collisionInfo.gameObject == rightColliderObject) {
+			transitionInEffect = false;
+		}
 	}
 }
