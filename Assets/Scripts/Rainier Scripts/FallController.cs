e59ce1c8a8f60c6b2e09233ab83c999422aa4fa9
Boomer42 <rainier.robichaud@gmail.com>
2014-07-06 15:35:11 +0100

    Floor Respwn

---
+++ Assets/Scripts/Rainier Scripts/FallController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallController : MonoBehaviour 
+{
+	private GameObject player;
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindWithTag("Player");
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Floor") 
+		{
+			player.transform.position = new Vector3(-0.6F,1.18F,1F);	
+		}
+	}
+}
