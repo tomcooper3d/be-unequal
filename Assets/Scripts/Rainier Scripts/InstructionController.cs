1979e6f4faf59024b8fdf5fc3ec438441b314837
Boomer42 <rainier.robichaud@gmail.com>
2014-07-06 21:50:38 +0100

    Controls

---
+++ Assets/Scripts/Rainier Scripts/InstructionController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionController : MonoBehaviour 
+{
+	private bool correct;
+	// Use this for initialization
+	void Start () 
+	{
+		correct = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.anyKeyDown) 
+		{
+			Debug.Log ("hit a key");
+
+
+			if (Input.GetKeyDown (KeyCode.A)) 
+			{
+				correct = true;
+				MoveControls ();
+				return;
+			}
+			if (Input.GetKeyDown (KeyCode.S)) 
+			{
+				correct = true;
+				MoveControls ();
+				return;
+			}
+			if (Input.GetKeyDown (KeyCode.W)) 
+			{
+				correct = true;
+				MoveControls ();
+				return;
+			}
+			if (Input.GetKeyDown (KeyCode.D)) 
+			{
+				correct = true;
+				MoveControls ();
+				return;
+			}
+			if (Input.GetKeyDown (KeyCode.Q)) 
+			{
+				correct = true;
+				MoveControls ();
+				return;
+			}
+			if (Input.GetKeyDown (KeyCode.E)) 
+			{
+				correct = true;
+				MoveControls ();
+				return;
+			}
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				correct = true;
+				MoveControls ();
+				return;
+			}
+			correct = false;
+			MoveControls ();
+		} 
+	}
+
+	void MoveControls()
+	{
+		if (correct == false) 
+		{
+			iTween.MoveTo (gameObject, iTween.Hash ("z", -7.8F, "easetype", "easeOutBounce"));
+		} 
+		if (correct == true)
+		{
+			iTween.MoveTo (gameObject, iTween.Hash ("z", -22F, "easetype", "easeOutBounce"));
+		}
+	}
+}
