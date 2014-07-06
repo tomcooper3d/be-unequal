a959a96f43549dcea7d9957d7355d758e66baa75
Connor Bell <connor.n.bell@gmail.com>
2014-07-06 18:20:29 +0100

    CAMERA ROTATE

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -4,14 +4,46 @@ using System.Collections;
 public class CameraRotate : MonoBehaviour {
 
 	public GameObject dd;
+	private bool inTransition;
+	private bool currentAngle;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	private float initalYAngle = 315.0f;
+	private float currentYAngle = 315.0f;
+
 	// Update is called once per frame
 	void Update () {
-		transform.RotateAround(dd.transform.position, Vector3.up, 60 * Time.deltaTime);
+		if (!inTransition) {
+			if (Input.GetKeyDown (KeyCode.Q)) {
+				inTransition = true;
+				StartCoroutine(rotate(true));
+			}
+			if (Input.GetKeyDown (KeyCode.E)) {
+				inTransition = true;
+				StartCoroutine(rotate(false));
+			}
+		}
+	}
+
+	IEnumerator rotate(bool left) {
+		bool enabled = true;
+		float last = transform.localEulerAngles.y;
+		float dest = (transform.localEulerAngles.y + (left ? 90 : -90)) % 360;
+		float total = 0.0f;
+
+		while (enabled) {
+			float deltaAngle = (left ? 150.0f : -150.0f) * Time.deltaTime;
+			
+			if (total > 90) {
+				deltaAngle = dest - last;
+				enabled = false;
+			}
+
+			total += Mathf.Abs(deltaAngle);
+			transform.RotateAround (dd.transform.position, Vector3.up, deltaAngle);
+			last = transform.localEulerAngles.y;
+			yield return null;
+		}
+
+		inTransition = false;
 	}
 }
