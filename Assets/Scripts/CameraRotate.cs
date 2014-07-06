874963eb2ac10710e554a313fcfdfc23c2a635c2
Connor Bell <connor.n.bell@gmail.com>
2014-07-06 15:23:24 +0100

    stuff

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+
+	public GameObject dd;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(dd.transform.position, Vector3.up, 60 * Time.deltaTime);
+	}
+}
