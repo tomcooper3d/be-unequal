ef39004db48108079108e6051953730a6a522787
Connor Bell <connor.n.bell@gmail.com>
2014-07-06 11:49:29 +0100

    added audio listener script thang

---
+++ Assets/Scripts/Sphereaudiolistener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphereaudiolistener : MonoBehaviour {
+
+	public GameObject sphere;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		this.transform.position = sphere.transform.position;
+	}
+}
